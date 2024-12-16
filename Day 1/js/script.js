8c67d063032a33d219cfa53923db6275225e6367
Nitin Kumar <nitinkumarpythonic@gmail.com>
2024-12-16 19:47:26 +0530

    first day complete files

---
+++ Day 1/js/script.js
@@ -0,0 +1,140 @@
+// var x = 5;
+// var y = 6;
+
+// alert(x + y);
+
+// Delete if not told ------------------------------
+// document.getElementById("result").innerHTML("Result is ",$(x + y));
+// -------------------------------------------------
+
+// Variables
+
+var VAT = 20;
+var calculatedVAT = (1 + VAT * 1/100);
+var netPriceOfShoes = 100;
+var netPriceOfTV = 959;
+
+var grossPriceOfShoes = netPriceOfShoes * calculatedVAT;
+var grossPriceOfTV = netPriceOfTV * calculatedVAT;
+
+// alert(grossPriceOfShoes);
+// alert(grossPriceOfTV);
+
+// Types of variables
+
+/*
+Numbers - integer 
+String - continuos characters
+Boolean - true or false 
+
+
+*/
+
+// --------------------------------------
+
+// THE SIZE OF LETTERS INSIDE VARIABLE NAME MATTERS !!!
+
+/*  
+* 1) You can't start variable name with the digit.
+  2) You can't use keywords
+  3) Don't use spaces
+  4) don't use operators in name
+*/ 
+
+// ----------------------------------------
+
+const PI = 3.14159;
+
+// PI = 3;
+
+// alert(PI)
+// ;
+
+// ----------------------------------------
+
+// Arithmetic operators
+
+/*
+
++ addition
+- substraction
+* multiplication
+/ division
+
+% remainder/modulous
+= assigning value
+
++=
+-=
+/=
+*=
+%=
+
+++ increment
+-- decrement
+x++ post incrementation
+x-- post decrementation
+++x pre incrementation
+--x pre decrementation
+
+*/
+
+// var a = 10;
+// var b = 2;
+// alert(a+b);
+// alert(a-b);
+// alert(a*b);
+// alert(a/b);
+
+// alert(a%b);
+// alert(a++);
+// alert(a--);
+
+// ------------------------------------------------
+
+// Relational Operators
+
+// == 
+// === identical, must be of same type too
+// != not equal to
+
+// var a = 5;
+// var b = 6;
+// var c = '2';
+// var d = 2;
+// alert( a===b );
+// alert(c == d);
+// alert(c === d);
+// alert(a != b);
+
+// -----------------------------------------------
+
+// Logical operators
+
+// 0 false, 1 true
+
+// ! not 
+// !(0) results to 1
+// !(1) results to 0
+
+// && conjunction
+// || disjunction or alternative 
+
+// conjunction is true only if both expressions are true 
+// disjunction is false only if both expressions are false
+
+// var a = 7;
+//     b = 3;
+
+// if (a>b)
+//   alert("test");
+
+// if (!(a>b || b==5))
+//   alert("best");
+
+// ------------------------------------------------------
+
+// Conditional statements
+
+
+
